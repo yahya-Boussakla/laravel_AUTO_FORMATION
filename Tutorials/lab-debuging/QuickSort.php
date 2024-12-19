74788b057f0d1258eaeb870c04aec8cc7bea5be8
yahya <yahyaboussakl@gmail.com>
2024-12-19 10:24:23 +0100

    lab php debuging

---
+++ Tutorials/lab-debuging/QuickSort.php
@@ -0,0 +1,29 @@
+<?php
+
+function quickSort($arr) {
+    if (count($arr) <= 1) {
+        return $arr;
+    }
+
+    $pivot = $arr[0];
+    $left = [];
+    $right = [];
+
+    foreach ($arr as $key => $value) {
+        if ($key === 0) continue; 
+        if ($value < $pivot) {
+            $left[] = $value;
+        } else {
+            $right[] = $value;
+        }
+    }
+
+    array_merge(quickSort($left), [$pivot], quickSort($right));
+}
+
+// Test de l'algorithme
+
+$array = [5, 3, 7, 2, 8, 5];
+$sorted = quickSort($array);
+
+echo "Tableau trié : " . implode(", ", $sorted);
